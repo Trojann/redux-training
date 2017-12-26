f3bc36a9c6da11f22364d235178d9ea346a74640
Thu Vu Xuan <thuvuxuan@vccorp.vn>
2017-12-26 11:32:29 +0700

    refactor code

---
+++ public/middlewares/index.js
@@ -0,0 +1,42 @@
+//middleware
+const logger = function(store) {
+  return function(next) {
+    return function(action) {
+      console.group('logger');
+      console.log('currentState == ', store.getState());
+      console.info('next(action) //action ==', action);
+      next(action);
+      console.log('nextState == ', store.getState());
+      console.groupEnd('logger');
+    }
+  }
+}
+
+const crashReporter = function(store) {
+  return function(next) {
+    return function(action) {
+      try {
+        next(action);
+      }
+      catch(err) {
+        console.group('crashReporter');
+        console.error('An error was happened with action: ', action);
+        console.error(err);
+        console.groupEnd('crashReporter');
+      }  
+    }
+  }
+}
+
+const thunk = function(store) {
+  return function(next) {
+    return function(action) {
+      if (typeof action === 'function') {
+        action(store.dispatch, store.getState());
+      }else{
+        next(action);
+      }
+    }
+  }
+}
+
